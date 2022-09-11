51a98396af2d58c740ee8ad1e61526a644026c73
“kaylamukai” <kayla_mukai@brown.edu>
2022-09-11 17:51:33 -0400

    done

--- app.js
+++ app.js
@@ -26,14 +26,20 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
-  // TODO: Create a new HTML element to display your data
+  const extract = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15);
+
+  const mapped = extract.map(extract => extract.name)
+
+  // TODO: Create a new HTML element to display your data: done in index.html
 
   // TODO: Append your new element to the page
+  const element = document.getElementById("new");
+  element.append("Name of all fruits whose sugar > 15: ", mapped.join(', '));
 
 }
 
